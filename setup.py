7a788932a9df8c45c461ff1fb23856f700a63f86
leereek@gmail.com <leereek@gmail.com>
2018-05-27 18:40:48 +0200

    version 0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='guiPackage',
-    version='0.1',
+    version='0.2',
     packages=find_packages(),
     license='MIT',
     description='Example python package',
